572b921c90debfa0e2f8080d5204b835faa5ff2d
Martin Lopez <martinlopez1305@gmail.com>
2022-04-21 18:13:12 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio-1/Modelos/Alumnos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1.Modelos
+{
+    public enum TipoDocumento { DNI, DU, Pasaporte }
+
+    public class Alumnos
+    {
+        public long NroDocumento { get; set; }
+        public TipoDocumento TipoDoc { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
